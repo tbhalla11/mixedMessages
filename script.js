2f33b8ebb1111b861f3dac76a262f1fdf0cee34c
Tarun Bhalla <tarunbhalla96@gmail.com>
2021-05-11 16:13:45 -0400

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let dailyReminder = ["Remember to stretch every hours", "Don't forget to drink water", "Exercising for 30 minutes a day is great for health", "Don't get too busy and forget to eat!", "Take a moment to breathe", "Make the best of your day!"]
 
 
