e686b3cc5aa7f3a660babd07206cf7a3b32d2998
Tarun Bhalla <tarunbhalla96@gmail.com>
2021-05-11 13:53:37 -0400

    Mixed messages.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+let array = ["Remember to stretch every hours", "Don't forget to drink water", "Exercising for 30 minutes a day is great for health", "Don't get too busy and forget to eat!", "Take a moment to breathe", "Make the best of your day!"]
+
+let randomGen = (Math.floor(Math.random()* array.length))
+
+console.log(array[randomGen])
